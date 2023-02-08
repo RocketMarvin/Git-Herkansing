c1095e22b740cb5f29d513f9f459824116bb0401
RocketMarvin <jonkermarvin1@gmail.com>
2023-02-08 21:50:43 +0100

    Endscreen
    heb een aanpassing gemaakt waar je een eindscherm krijgt als je zoveel punten hebt

--- Unity Git herkansing/Assets/Scripts/Points.cs
+++ Unity Git herkansing/Assets/Scripts/Points.cs
@@ -4,14 +4,35 @@ using UnityEngine;
 
 public class Points : MonoBehaviour
 {
+    ScoreScript MyscoreScript;
+    public int score = 0;
+
+    public void start()
+    {
+        MyscoreScript = GetComponent<ScoreScript>();
+        Endscreen();
+    }
+
+
 
     public void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Point"))
+        if (other.tag == "Point")
         {
+            score++;
             GameObject.Find("Canvas").GetComponent<ScoreScript>().pointScore();
             Destroy(other.gameObject);
+            if (score == 8)
+            {
+                Endscreen();
+            }
+
         }
+    }
+
+    public void Endscreen()
+    {
+       GameObject.Find("Win Canvas").GetComponent<Canvas>().enabled = true;
     }
 
 }
